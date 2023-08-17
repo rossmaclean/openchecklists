83742c45d57703a6aa31da52cccf4c9b4febd4a4
Ross MacLean <ross@rossmac.co.uk>
2023-08-17 16:15:40 +0100

    Add basic display of checklist.

---
+++ src/components/checklist/Checklist.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Checklist from './Checklist';
+
+test('renders learn react link', () => {
+  render(<Checklist />);
+  const linkElement = screen.getByText(/checklist/i);
+  expect(linkElement).toBeInTheDocument();
+});
